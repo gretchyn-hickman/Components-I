108e21a572f6c59775bf89be8d924ce42a1570fa
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-05-11 14:41:11 -0600

    problem solving

--- src/index.js
+++ src/index.js
@@ -2,51 +2,7 @@ import "./index.less";
 import "./components/article/article.js";
 import "./components/menu/menu.js";
 
-const testData = {
-  title: "test",
-  date: "test",
-  firstParagraph: `test`,
-  secondParagraph: `test`,
-  thirdParagraph: `test`,
-};
 
-function makeArtical( {title, date, firstParagraph, secondParagraph, thirdParagraph,} ) {
-  const articalDiv = document.createElement("div");
-  articalDiv.classList.add("article");
-  const articalTitle = document.createElement("h2");
-  const articalDate = document.createElement("p");
-  const articalOne = document.createElement("p");
-  const articalTwo = document.createElement("p");
-  const articalThree = document.createElement("p");
-  const expandButton = document.createElement("span");
-  expandButton.classList.add("expandButton");
-
-  articalTitle.textContent = title;
-  articalDate.textContent = date;
-  articalOne.textContent = firstParagraph;
-  articalTwo.textContent = secondParagraph;
-  articalThree.textContent = thirdParagraph;
-  expandButton.textContent = 'expand'
-
-
-  articalDiv.appendChild(articalTitle);
-  articalDiv.appendChild(articalDate);
-  articalDiv.appendChild(articalOne);
-  articalDiv.appendChild(articalTwo);
-  articalDiv.appendChild(articalThree);
-  articalDiv.appendChild(expandButton);
-  return articalDiv;
-}
-const articalOne = makeArtical(testData);
-
-
-const placeArticals = document.querySelector('.articles');
-placeArticals.appendChild(articalOne);
-
-
-
-console.log(testData);
-console.log(articalOne);
 
 /* <div class="article">
     <h2>{title of the article}</h2>
